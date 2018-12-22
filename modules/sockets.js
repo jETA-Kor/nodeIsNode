b498216325425dab43afec1963e8604d1ab2a069
jETA <jeta@jetalog.net>
2018-12-22 11:13:37 +0900

    누락된 주석 추가

--- modules/sockets.js
+++ modules/sockets.js
@@ -1,7 +1,7 @@
 function Sockets() {
-    this.list = [];
+    this.list = []; // 소켓 배열
 }
-Sockets.prototype.add = function (socket) {
+Sockets.prototype.add = function (socket) { // 소켓 추가
     this.list.push(socket);
 
     var self = this;
@@ -9,13 +9,13 @@ Sockets.prototype.add = function (socket) {
         self.remove(socket);
     });
 }
-Sockets.prototype.remove = function (socket) {
+Sockets.prototype.remove = function (socket) { // 소켓 제거
     var i = this.list.indexOf(socket);
     if (i != -1) {
         this.list.splice(i, 1);
     }
 }
-Sockets.prototype.emit = function (name, data, except) {
+Sockets.prototype.emit = function (name, data, except) { // 배열의 소켓에만 메시지 전송
     var i = this.list.length;
     while (i--) {
         if (this.list[i] != except) {
